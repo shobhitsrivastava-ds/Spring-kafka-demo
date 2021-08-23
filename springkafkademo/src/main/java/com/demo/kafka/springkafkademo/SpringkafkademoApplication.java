a6999629c2a8632b8b76938a3c447634436cf185
shobhitsrivastava-ds <shobhitsrm18th@gmail.com>
2021-08-23 16:56:41 +0530

    Added new file

---
+++ springkafkademo/src/main/java/com/demo/kafka/springkafkademo/SpringkafkademoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.kafka.springkafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkafkademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkafkademoApplication.class, args);
+	}
+
+}
---
+++ springkafkademo/src/test/java/com/demo/kafka/springkafkademo/SpringkafkademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.kafka.springkafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkafkademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
